463ead9e56a6e9d2e6d80c946bd2b0210e556595
Marshall Vielmetti <mvielmetti@outlook.com>
2025-07-18 13:22:27 -0400

    updated output format'

--- examples/caams_demo/compute_trajectory.jl
+++ examples/caams_demo/compute_trajectory.jl
@@ -1,7 +1,8 @@
 using ArgParse
 
+include("../3d_example_utils.jl")
+include("file_util.jl")
 include("demo_util.jl")
-using .DemoUtil
 
 function parse_commandline()
     s = ArgParseSettings()
@@ -25,34 +26,28 @@ function main()
     println("")
 
     @info "Solving for RRT* Path"
-    if !(DemoUtil.solve_leader_path(env))
+    if !(DemoUtil.solve_leader_path!(env))
         @warn "Failed to solve an RRT* Trajectory. Exiting"
         return 1
     end
     @info "RRT Path Constructed successfully."
-    println("")
 
     @info "Solving Inter-Agent Gatekeeper Problem...this may take a while."
     if !DemoUtil.solve_gk_problem!(env)
         @warn "Failed to solve gatekeeper problem."
         return 2
     end
-    @info "Success!"
-    println("")
 
     @info "Saving intermediate result to file..."
 
-    result_file_name = "scenarios/$(env.scenario.name)_result.csv"
+    result_file_name = "caams_demo/scenarios/$(env.scenario.name)_result.csv"
     DemoUtil.write_to_file(env, result_file_name)
     @info "Results written to $result_file_name"
-    println("")
 
     @info "Fitting polynomials to the final trajectories."
-    polynomials = DemoUtil.fit_polynomials!(env.data)
-    @info "Success! Writing to file."
+    polynomials = DemoUtil.fit_polynomials(env.data)
 
-
-    trajectory_file_name = "scenarios/$(env.scenario.name)_trajectories.csv"
+    trajectory_file_name = "caams_demo/scenarios/$(env.scenario.name)_trajectories.csv"
     DemoUtil.write_polynomials_to_file(polynomials, trajectory_file_name)
     @info "Trajectories written to $trajectory_file_name"
 end
